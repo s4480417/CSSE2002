26d37130c2d249e621ffde51d49503819bb20f47
Miguel Valencia <valencimm@gmail.com>
2018-09-13 11:22:20 +1000

    Week 2

--- Practicals/Prac2.java
+++ Practicals/Prac2.java
@@ -1,4 +1,16 @@
 public class Prac2
 {
-
+    public static boolean isPalindrome1(String s)
+    {
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome1("RACECAR"));
+    }
 }
--- Tutorials/Tute2.java
+++ Tutorials/Tute2.java
@@ -1,4 +1,45 @@
 public class Tute2
 {
-
+    public static String num(int x) {
+        switch (x) {
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+            default:
+                return "??";
+        }
+    }
+    public static int Fib(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+    public static long A(short m, short n) {
+        if (m == 0) {
+            return (long)(n + 1);
+        } else if (m > 0 && n == 0) {
+            return (long)(A((short)(m - 1), (short)1));   
+        } else if (m > 0 && n > 0) {
+            return (long)((short)A((short)(m - 1), (short)A(m, (short)(n - 1))));
+        } else {
+            return -1;
+        } 
+    }
 }
